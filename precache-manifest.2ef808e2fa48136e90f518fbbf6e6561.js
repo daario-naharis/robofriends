526a0e08e7e11269b78823ed69b121e9c6479f7b
daario-naharis <arpitbaranwal410@gmail.com>
2020-06-28 17:40:06 +0530

    Updates

---
+++ precache-manifest.2ef808e2fa48136e90f518fbbf6e6561.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8525366fe49a9cd960be33ebedbe1f6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919e3dfbe39459276195",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1e29abfdcd930e1532f2",
+    "url": "/static/css/main.8093198e.chunk.css"
+  },
+  {
+    "revision": "919e3dfbe39459276195",
+    "url": "/static/js/2.e636b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e636b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e29abfdcd930e1532f2",
+    "url": "/static/js/main.e1ff8db9.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
